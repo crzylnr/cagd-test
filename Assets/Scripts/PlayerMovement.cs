d136ed9eafb232c8d8755a9f1908b130e40d0a8d
CAGD Student <cagd495@mailinator.com>
2015-08-27 15:50:36 -0700

    Platform relative Movement and player death not working ...

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,26 +3,64 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float moveSpeed = 2.5f;
+    public float negMoveSpeed = -2.5f;
+	public float gravity = 4.0f;
+	public float jumpSpeed = 5.0f;
+
+	private Vector3 playerMovement = Vector3.zero;
+
+
+	void start (){
+  
+	}
+  //supposed to destroy the player object ... not working 
+    void OnCollisionEnter(Collision col) {
+     //outputs col's name into console
+        Debug.Log("collision name = " + col.gameObject.name);
+     //destroys playerobject when col's name is "Enemy"
+        if(col.gameObject.name == "Enemy")
+            Destroy(gameObject);
+    }
+
+	void Update (){
+        
+		CharacterController control = GetComponent<CharacterController> ();
+
+
+      //checks if player is grounded and will allow player to jump
+        if(control.isGrounded) {
+            if(Input.GetKey(KeyCode.Space)) {
+                playerMovement.y = jumpSpeed;
+            } 
+        } 
+
+     //move left NOT WORKING *** Fixed 
+    //movement left and right
+        if(Input.GetKey(KeyCode.A)) {
+            Debug.Log("AAAAAAAAAA");
+            playerMovement.x = negMoveSpeed;
+        } else if(Input.GetKey(KeyCode.D)) {
+            playerMovement.x = moveSpeed;
+        } else { playerMovement.x = 0; }
+
+     //Applies gravity to player 
+		playerMovement.y -= gravity * Time.deltaTime;
+        control.Move(playerMovement * Time.deltaTime);
+
+
+
+
+
+	}
     /*
      * TO DO:
-     * Movement
-     *      Should use the CharacterController which is already attached to this GameObject
-     *      Be able to move left and right
-     *      Collide with/be stopped by walls
-     *      Not move too quickly or slowly
-     *          Remember that movement happens every frame
-     * Jumping/Falling
-     *      Fall to the ground, and not through it
-     *      Able to jump
-     *      Can reach platforms to the right, but not the one on the left
-     *      Only able to jump while standing on the ground
-     * Input
-     *      Ideally, use the KeyboardInput script which is already attached to this GameObject
-     *      A & D for left and right movement
-     *      Space for jumping
+     * 
+     * 
      * Moving Platform
      *      When standing on the platform, the character should stay on it/move relative to the moving platform
      *      When not standing on the platform, revert to normal behavior
+     *      
      * Enemy
      *      If the character touches the enemy, he should "die"
      *      
